f398a937c465887129942fd7a3095a8efc2ec685
Koen de Bont <kaj.debont@student.avans.nl>
2017-11-23 16:26:12 +0100

    get request added

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -4,33 +4,59 @@ import { Subject } from 'rxjs/Subject';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/Rx';
 
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
-    new Recipe(
-      'Tasty Schnitzel',
-      'A super-tasty Schnitzel - just awesome!',
-      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]),
-    new Recipe('Big Fat Burger',
-      'What else you need to say?',
-      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
-      [
-        new Ingredient('Buns', 2),
-        new Ingredient('Meat', 1)
-      ])
+    // new Recipe(
+    //   'Tasty Schnitzel',
+    //   'A super-tasty Schnitzel - just awesome!',
+    //   'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+    //   [
+    //     new Ingredient('Meat', 1),
+    //     new Ingredient('French Fries', 20)
+    //   ]),
+    // new Recipe('Big Fat Burger',
+    //   'What else you need to say?',
+    //   'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+    //   [
+    //     new Ingredient('Buns', 2),
+    //     new Ingredient('Meat', 1)
+    //   ])
   ];
 
-  constructor(private slService: ShoppingListService) {}
+  constructor(private slService: ShoppingListService,
+              private httpClient: HttpClient) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
-    return this.recipes.slice();
+    this.httpClient.get<Recipe[]>('https://jsonplaceholder.typicode.com/posts/1/comments', {
+      observe: 'body',
+      responseType: 'json'
+    })
+    .map(
+      (recipes) => {
+        for (let recipe of recipes) {
+          if (!recipe['ingredients']) {
+            recipe['ingredients'] = [];
+          }
+        }
+        return recipes;
+      }
+    )
+    .subscribe(
+      (recipes: Recipe[]) => {
+        this.setRecipes(recipes);
+      }
+    );
   }
 
   getRecipe(index: number) {
